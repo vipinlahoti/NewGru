4e46303ac4f5df22177b0f8b160ba515bbafd48e
Vipin Lahoti <vipinl@ispace.com>
2018-01-30 15:14:37 +0530

    flush packages

--- packages/grudr/lib/server/hospitals/indexes.js
+++
@@ -1,4 +0,0 @@
-import { Hospitals } from '../../modules/hospitals/index.js';
-
-Hospitals._ensureIndex({"status": 1, "isFuture": 1});
-Hospitals._ensureIndex({"status": 1, "isFuture": 1, "postedAt": 1});
--- packages/vulcan-payments/lib/modules/index.js
+++
@@ -1,9 +0,0 @@
-export Charges from './charges/collection.js';
-
-import '../components/Checkout.jsx';
-
-import './routes.js';
-import './i18n.js';
-import './custom_fields.js';
-
-export * from './products.js'
