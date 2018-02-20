a07fdfff9916ddb018360ec9d8abf024a081a1a2
vipinlahoti <vipi.nsl2787@gmail.com>
2018-02-20 23:48:32 +0530

    dropdown fixed

--- packages/grudr/lib/components/users/UsersMenu.jsx
+++ packages/grudr/lib/components/users/UsersMenu.jsx
@@ -1,41 +1,49 @@
 import { Components, registerComponent, withCurrentUser } from 'meteor/vulcan:core';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'meteor/vulcan:i18n';
 import { Meteor } from 'meteor/meteor';
-import Dropdown from 'react-bootstrap/lib/Dropdown';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-import { LinkContainer } from 'react-router-bootstrap';
 import Users from 'meteor/vulcan:users';
 import { withApollo } from 'react-apollo';
+import { Link } from 'react-router';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const UsersMenu = ({currentUser, client}) =>
-  <div className="users-menu">
-    <Dropdown id="user-dropdown">
-      <Dropdown.Toggle>
-        <Components.UsersAvatar size="small" user={currentUser} addLink={false} />
-        <div className="users-menu-name">{Users.getDisplayName(currentUser)}</div>
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <LinkContainer to={`/users/${currentUser.slug}`}>
-          <MenuItem className="dropdown-item" eventKey="1"><FormattedMessage id="users.profile"/></MenuItem>
-        </LinkContainer>
-        <LinkContainer to={`/account`}>
-          <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="users.edit_account"/></MenuItem>
-        </LinkContainer>
+class UsersMenu extends PureComponent {
+  constructor(props) {
+    super(props);
 
-        <Components.ShowIf
-          check={() => Users.isAdmin(currentUser)}
-        >
-          <LinkContainer to={`/admin`}>
-            <MenuItem className="dropdown-item" eventKey="2">Admin</MenuItem>
-          </LinkContainer>
-        </Components.ShowIf>
-        <MenuItem className="dropdown-item" eventKey="4" onClick={() => Meteor.logout(() => client.resetStore())}><FormattedMessage id="users.log_out"/></MenuItem>
-      </Dropdown.Menu>
-    </Dropdown>
-  </div>
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
 
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    const {currentUser, client} = this.props;
+
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          <Components.UsersAvatar size="small" user={currentUser} link={false} />
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem tag={Link} to={`/users/${currentUser.slug}`}><FormattedMessage id="users.profile"/></DropdownItem>
+          <DropdownItem tag={Link} to={`/account`}><FormattedMessage id="users.edit_account"/></DropdownItem>
+          <Components.ShowIf check={() => Users.isAdmin(currentUser)}>
+            <DropdownItem tag={Link} to={`/admin`}>Admin</DropdownItem>
+          </Components.ShowIf>
+          <DropdownItem tag={Link} onClick={() => Meteor.logout(() => client.resetStore())}><FormattedMessage id="users.log_out"/></DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    )
+  }
+}
 
 UsersMenu.propsTypes = {
   currentUser: PropTypes.object,
